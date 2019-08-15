9cb67485095beb93ab2e29578c84baeb7a9ebc26
sylar <24067539@qq.com>
2019-08-15 14:23:42 +0800

    v1

--- test/dnn.py
+++ test/dnn.py
@@ -10,25 +10,30 @@ from keras.optimizers import Adam
 import tensorflow as tf
 import numpy as np
 import keras.backend.tensorflow_backend as KTF
+import keras
+from keras.layers import LeakyReLU
 
 from data import threadsafe_generator
 
 
-def process(x, k0, k1, k2,):
+def process(x, k0, k1, k2):
     # return k0 + k1 * (x+k0) + k2 * (x+k0) ** 2 + k3 * (x+k0) ** 3 + k4 * (x+k0) ** 4 + k5 * (x+k0) ** 5
     # s11 = (-1 + random.randint(0, 1))
     choice = random.random()
-    if choice > 0.3:
-        return k1 * x + k2 * (x + random.randint(-1080, 1080)) ^ 2 + k0 * random.randint(-1920, 1920)
+    # if choice > 0.8:
+    #     return k3 * (x + random.randint(-1080, 1080)) ** 3 + k0 * random.randint(-1920, 1920)
+    # el
+    if choice > 0.2:
+        return k1 * x + k2 * (x + random.randint(-1920, 1920)) ** 2 + k0 * random.randint(-1080, 1080)
     else:
-        return k1 * (x + random.randint(-1080, 1080)) + k0 * random.randint(-1920, 1920)
+        return k1 * (x + random.randint(-1920, 1920)) + k0 * random.randint(-1080, 1080)
 
-def getY(x, k0, k1, k2, k3, k4, k5):
-    return k0 + k1 * (x + k0) + k2 * (x + k0) ** 2 + k3 * (x + k0) ** 3 + k4 * (x + k0) ** 4 + k5 * (x + k0) ** 5
+# def getY(x, k0, k1, k2, k3, k4, k5):
+#     return k0 + k1 * (x + k0) + k2 * (x + k0) ** 2 + k3 * (x + k0) ** 3 + k4 * (x + k0) ** 4 + k5 * (x + k0) ** 5
 
 
 @threadsafe_generator
-def frame_generator(batch_size):
+def frame_generator(batch_size, withMax=False):
     """Return a generator that we can use to train on. There are
     a couple different things we can return:
 
@@ -38,83 +43,56 @@ def frame_generator(batch_size):
     # Get the right dataset for the generator.
     # train, test = self.split_train_test()
     while 1:
-        # b_tensor_lable = np.zeros((batch_size, 10, 10, 2))
         b_tensor_input = np.zeros((batch_size, 10, 10, 2))
 
         b_xy_lable = np.zeros((batch_size, 10, 20))
-        # b_xy_input = np.zeros((batch_size, 10, 20))
         for b in range(0, batch_size):
 
-            # xy_lable = []
-            # xy_input = []
-            ran = random.randint(2, 5)
-            for i in range(0, 10):
-                k0 = random.random()
-                k1 = random.random()
-                k2 = random.random()
-                k3 = random.random()
-                k4 = random.random()
-                k5 = random.random()
+            for person_i in range(0, 10):
+                k0 = random.random()*(-1 if random.randint(0, 1) == 0 else 1)
+                k1 = random.random()*(-1 if random.randint(0, 1) == 0 else 1)
+                k2 = random.random()*(-1 if random.randint(0, 1) == 0 else 1)
 
-                # y = getY(x, k0, k1, k2 if i in range(2, 10) else 0, k3 if i in range(4, 10) else 0, k4 if i in range(6, 10) else 0, k5 if i in range(8, 10) else 0)
-                ran_5 = 0  # random.randint(0, 1)
-                if ran_5 == 0:
-                    if i in (2, 3, 4, 5):
-                        # x = np.random.rand(10)
-                        rangeLen = random.randint(50, 1080)
-                        x = np.random.rand(10) * rangeLen + np.random.randint(1, 1080 - rangeLen)
-                        # x = np.random.rand(10) * np.random.randint(1, 1080)
-                        x.sort()
+                # x = np.random.rand(10)
+                # rangeLen = random.randint(50, 1920-1)
+                # x = np.random.rand(10) * rangeLen + np.random.randint(0, 1920 - rangeLen)
+                x = np.random.rand(10) * np.random.randint(1079, 1080)
+                x.sort()
+                y = process(x, k0, k1, k2)
 
-                        y = getY(x, k0, k1, k2 if i == k2 else 0, k3 if i == k3 else 0, k4 if i == k4 else 0, k5 if i == k5 else 0)
-                        # y = getY(x, k0, k1, k2, k3, 0, 0)
-                    else:
-                        # x = np.random.rand(10)
-                        rangeLen = random.randint(50, 100)
-                        x = np.random.rand(10) * rangeLen + np.random.randint(1, 1080 - rangeLen)
-                        # x = np.random.rand(10) * np.random.randint(1, 1080)
-                        x.sort()
-
-                        y = getY(x, k0, k1, k2 if ran == k2 else 0, k3 if ran == k3 else 0,
-                                 k4 if ran == k4 else 0, k5 if ran == k5 else 0)
-                else:
-                    x = np.random.rand(10)
-                    y = getY(x, k0, k1, k2, k3, k4, k5)
                 # Normalize
                 # t_min = min(x.min(), y.min())
                 # t_max = max(x.max(), y.max())
-                # x = x/t_max
-                # y = y/t_max
-
-                b_tensor_input[b, i, :, 0] = x
-                b_tensor_input[b, i, :, 1] = y
-                # # shuffle exclude first one
-                # points = list(zip(x, y))[1:]
-                # random.shuffle(points)  # TODO np.random.shuffle() instead
-                # points = list(zip(*points))
-                # # xy_input.append(points[0])
-                # # xy_input.append(points[1])
+                # x = (x-x.min())/(x.max()-x.min())
+                # y = (y-y.min())/(y.max()-y.min())
                 #
-                # b_xy_input[b, i, 0] = np.asarray(x[0])
-                # b_xy_input[b, i, 10] = np.asarray(y[0])
-                # b_xy_input[b, i, 1:10] = np.asarray(points[0])
-                # b_xy_input[b, i, 11:20] = np.asarray(points[1])
-                #
-                # # xy_lable.append(np.array(x))
-                # # xy_lable.append(np.array(y))
+                # b_temp
 
-                b_xy_lable[b, i, 0:10] = x[:]
-                b_xy_lable[b, i, 10:20] = y[:]
+                b_tensor_input[b, person_i, :, 0] = x
+                b_tensor_input[b, person_i, :, 1] = y
+
+                b_xy_lable[b, person_i, 0:10] = x[:]
+                b_xy_lable[b, person_i, 10:20] = y[:]
 
             # shuffle frames exclude first frame
-            for j in range(1, 10):
-                np.random.shuffle(b_tensor_input[b, :, j, :])
-
-        # b_xy_input = b_xy_input/b_xy_input.max()
-        yield b_tensor_input.swapaxes(1, 2).reshape((b + 1, 200)), b_xy_lable.reshape((b + 1, 200))
+            for frame_j in range(1, 10):
+                np.random.shuffle(b_tensor_input[b, :, frame_j, :])
+        # temp_min = b_xy_lable.min()
+        # temp_max = b_xy_lable.max()-temp_min
+        # b_xy_lable = (b_xy_lable-temp_min)/temp_max
+        # b_tensor_input = (b_tensor_input-temp_min)/temp_max
+        temp_max = b_xy_lable.max()
+        b_xy_lable = b_xy_lable / temp_max
+        b_tensor_input = b_tensor_input / temp_max
+        if withMax:
+            yield b_tensor_input.swapaxes(1, 2).reshape((b + 1, 200)), b_xy_lable.reshape((b + 1, 200)), temp_max
+        else:
+            yield b_tensor_input.swapaxes(1, 2).reshape((b + 1, 200)), b_xy_lable.reshape((b + 1, 200))
 
 
 def dnn(saved_weights=None):
+    # act = keras.layers.advanced_activations.PReLU(init='zero', weights=None)
+
     model = Sequential()
 
     # 1 Total params: 200,800
@@ -124,25 +102,33 @@ def dnn(saved_weights=None):
     # model.add(Dense(200, activation='relu'))
 
     # 2 Total params: 281,000
-    model.add(Dense(200, activation='relu', input_shape=(200,)))
-    model.add(Dense(600, activation='relu'))
+    model.add(Dense(200, input_shape=(200,)))
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(Dense(1200))
+    model.add(LeakyReLU(alpha=0.01))
     model.add(Dropout(0.1))
-    model.add(Dense(200, activation='relu'))
+    model.add(Dense(200))
 
     # 3 Total params: 361,200
-    # model.add(Dense(200, activation='relu', input_shape=(200,)))
-    # model.add(Dense(400, activation='relu'))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(400, activation='relu'))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(200, activation='relu'))
+    # model.add(Dense(200, input_shape=(200,)))
+    # model.add(LeakyReLU(alpha=0.01))
+    # model.add(Dense(600))
+    # model.add(LeakyReLU(alpha=0.01))
+    # model.add(Dense(800))
+    # model.add(LeakyReLU(alpha=0.01))
+    # model.add(Dense(800))
+    # model.add(LeakyReLU(alpha=0.01))
+    # model.add(Dense(600))
+    # model.add(LeakyReLU(alpha=0.01))
+    # model.add(Dense(200))
+
 
     if saved_weights is not None:
         print('load weights ', saved_weights)
         model.load_weights(saved_weights)
 
     # Now compile the network.
-    optimizer = Adam(lr=5e-5, decay=1e-3)
+    optimizer = Adam(lr=1e-3, decay=1e-3)
     model.compile(loss=tf.losses.huber_loss, optimizer=optimizer,
                   metrics=['accuracy'])
 
